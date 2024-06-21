4cca277a210ea1e8e6a82771323f0707e10d5ed4
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-06-21 13:42:14 +0200

    refructured my file

--- src/components/Episodes.jsx
+++ src/components/Episodes.jsx
@@ -1,14 +1,15 @@
 
+
 const Episode = ({ episode }) => {
-    return (
-      <li className="episode-item">
-        <div className="episode-details">
-          <h4 className="episode-title">{episode.title}</h4>
-          <p className="episode-duration">Duration: {episode.duration}</p>
-          {/* Implement favorite toggle button and functionality here */}
-        </div>
-      </li>
-    );
-  };
-  
-  export default Episode;
+  return (
+    <li className="episode-item">
+      <div className="episode-details">
+        <h4 className="episode-title">{episode.title}</h4>
+        <p className="episode-duration">Duration: {episode.duration}</p>
+        {/* Implement favorite toggle button and functionality here */}
+      </div>
+    </li>
+  );
+};
+
+export default Episode;
