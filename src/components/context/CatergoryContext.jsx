6364aec2256e2287432865d6d36a00e34ea10c03
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-06-21 13:01:36 +0200

    rovider for managing the selected category state across application

---
+++ src/components/context/CatergoryContext.jsx
@@ -0,0 +1,14 @@
+// CategoryContext.jsx
+import { createContext, useState } from 'react';
+
+export const CategoryContext = createContext();
+
+export const CategoryProvider = ({children}) => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  return (
+    <CategoryContext.Provider value={{ selectedCategory, setSelectedCategory }}>
+      {children}
+    </CategoryContext.Provider>
+  );
+};
