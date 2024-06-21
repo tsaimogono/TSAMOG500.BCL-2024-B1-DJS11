0b87b45fa5a20e347bd910219c442019179c5cf7
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-06-21 13:26:51 +0200

    file sets up a context to manage search query state within  application.

---
+++ src/contexts/SearchContext.jsx
@@ -0,0 +1,14 @@
+// contexts/SearchContext.js
+import React, { createContext, useState } from 'react';
+
+export const SearchContext = createContext();
+
+export const SearchProvider = ({ children }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  return (
+    <SearchContext.Provider value={{ searchQuery, setSearchQuery }}>
+      {children}
+    </SearchContext.Provider>
+  );
+};
