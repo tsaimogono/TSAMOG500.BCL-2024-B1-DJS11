5430857c26858076667f223ffcd8bcc89b97f977
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-06-21 16:34:16 +0200

    FIXED CONTEXT

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         {/* SVG logo with neon pink theme */}
         <svg className="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 50" width="200" height="50">
   <text x="5" y="35" fontSize="28" fontWeight="bold" fill="#f0c">MAGIC</text>
-  <text x="110" y="35" fontSize="28" fontWeight="bold" fill="#f0c">VOICE</text>
+  <text x="110" y="35" fontSize="28" fontWeight="bold" fill="#f0c">VOICES</text>
 </svg>
 
       </Link>
