856fb587d14043b84abfe7281bbcd856815d9662
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-06-21 13:31:36 +0200

    sets up the main structure of your React application using React Router for navigation and context providers for state management.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Yourfav from './components/YourFav';
+import Show from './components/Show';
+import Footer from './components/Footer';
+import { CategoryProvider } from './components/context/CatergoryContext';
+import { FavoritesProvider } from './contexts/FavouriteContext';
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <CategoryProvider>
+      <FavoritesProvider>
+        <Router>
+          <div>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/yourfav" element={<Yourfav />} />
+              <Route path="/show/:showId" element={<Show /> }/>
+            </Routes>
+            <Footer />
+          </div>
+        </Router>
+      </FavoritesProvider>
+    </CategoryProvider>
+  );
+};
 
-export default App
+export default App;
